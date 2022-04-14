419e3597baa102359d5d6cde3b8c95ea74924d3f
Nathan Verbruggen <naverbru@mi-r4-p3.s19.be>
2022-04-14 17:46:56 +0200

    here_doc debut

--- here_doc.c
+++ here_doc.c
@@ -1,27 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: naverbru <naverbru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/12 17:41:42 by naverbru          #+#    #+#             */
-/*   Updated: 2022/04/14 17:39:33 by naverbru         ###   ########.fr       */
+/*   Created: 2022/04/14 17:30:01 by naverbru          #+#    #+#             */
+/*   Updated: 2022/04/14 17:43:44 by naverbru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "libft/libft.h"
+#include "pipex.h"
 
-//utils.c
-int		ft_process(char *av, char **env);
-char	*ft_strjoin_pipex(char *s1, char *s2);
-void	ft_putstr(char *str);
-char	*ft_exit(char *msg);
+char	**here_doc(char *limiter)
+{
+	char	**tab = 0;
+	int		i;
+	int 	ret;
+	char	buf[100];
 
-char	**here_doc(char *limiter);
-
-//pipex.c
+	i = 0;
+	while (i < 100)
+	{
+		ret = read(0, &buf, 10);
+		buf[ret] = '\0';
+		printf("buf = %s", buf);
+		if (ft_strncmp(limiter, buf, ft_strlen(limiter)) == 0)
+		{
+			printf("termine");
+			break ;
+		}
+		i++;
+	}
+	return (tab);	
+}
--- pipex.c
+++ pipex.c
@@ -6,40 +6,12 @@
 /*   By: naverbru <naverbru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 14:44:00 by naverbru          #+#    #+#             */
-/*   Updated: 2022/04/14 16:25:10 by naverbru         ###   ########.fr       */
+/*   Updated: 2022/04/14 17:32:37 by naverbru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	first_process(char **av, char **env, int *fd)
-{
-	fd[0] = open(av[1], O_RDONLY);
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);	
-	dup2(fd[1], STDOUT_FILENO);
-	close(fd[1]);
-	ft_process(av[2], env);
-	return (1);
-}
-
-int	last_process(char **av, char **env, int *fd)
-{
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
-	close(fd[1]);
-	int	fd_out = open(av[4], O_WRONLY | O_TRUNC);
-	dup2(fd_out, STDOUT_FILENO);
-	close(fd_out);
-	ft_process(av[2], env);
-	return (1);
-}
-/*
-int middle_process(char **av, char **env, int *fd)
-{
-
-}
-*/
 int	forking(int n, char **av, char **env, int ac)
 {
 	int fd[2];
@@ -57,10 +29,10 @@ int	forking(int n, char **av, char **env, int ac)
 			return (-1);
 		if (pid == 0)
 		{
-			if (i == 2 && i + 1 != n)
+			if (i == 2)
 			{
 				fd[0] = open(av[1], O_RDONLY);
-				dup2(fd[0], STDIN_FILENO);
+				dup2(fd[0], STDIN_FILENO);	
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[1]);
 			}	
@@ -92,50 +64,11 @@ int	forking(int n, char **av, char **env, int ac)
 
 int	main(int ac, char **av, char **env)
 {
-	forking(ac - 1, av, env, ac);
-	ac = 4;
+	char **here;
 
+	if (ft_strncmp("here_doc", av[1], 8) == 0)
+		here = here_doc("limiter");
+	else
+		forking(ac - 1, av, env, ac);
 	return (0);
 }
-
-/*
-	if (pipe(fd) == -1)
-		return (-1);
-	pid1 = fork();
-	if (pid1 < 0)
-		return (-1);
-	if (pid1 == 0)
-		if (first_process(av, env, fd) != 1)
-			return(-1);	
-	pid2 = fork();
-	if (pid2 < 0)
-		return (-1);
-	if (pid2 == 0)
-		if (second_process(av, env, fd) != 1)
-			return (-1);
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-	return (1);
-}
-*/
-
-/*
-
-
-		if (i = 0)
-		{
-			if (pid == 0)
-				first_process(av, env, fd);
-		}
-		if (i = n - 1)
-		{
-			if (pid == 0)
-				last_process(av, env, fd);
-		}
-		else 
-		{
-			if (pid == 0)
-		}
-		*/
