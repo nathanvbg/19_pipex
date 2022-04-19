ed71f0e52af74c84f5fad47ae375fded0c77f6d4
Nathan Verbruggen <naverbru@mi-r7-p3.s19.be>
2022-04-19 13:06:41 +0200

    struct ajoutee

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: naverbru <naverbru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 14:44:00 by naverbru          #+#    #+#             */
-/*   Updated: 2022/04/19 12:27:39 by naverbru         ###   ########.fr       */
+/*   Updated: 2022/04/19 13:06:00 by naverbru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,38 +38,51 @@ int	middle_process(int fd_in, int fd_1)
 	return (1);
 }
 
+int	child_process(t_file x, char **av, int i, int n, int ac)
+{
+	if (i == 2)
+		first_process(x.fd[0], x.fd[1], av[1]);
+	else if (i + 1 == n)
+		last_process(x.fd_in, av[ac - 1]);
+	else
+		middle_process(x.fd_in, x.fd[1]);
+	close(x.fd[0]);
+	return (1);
+}
+
+int	parent_process(t_file x, int *i)
+{
+	close (x.fd[1]);
+	//x.fd_in = x.fd[0];
+	*i = *i + 1;
+	return (1);
+}
+
 int	forking(int n, char **av, char **env, int ac)
 {
-	int fd[2];
-	int pid;
-	int fd_in;
+	t_file x;
 	int	i;
-
+	
 	i = 2;
-	fd_in = 0;
+	x.fd_in = 0;
 	while (i < n)
 	{
-		pipe(fd);
-		pid = fork();
-		if (pid == -1)
+		pipe(x.fd);
+		x.pid = fork();
+		if (x.pid == -1)
 			return (-1);
-		if (pid == 0)
+		if (x.pid == 0)
 		{
-			if (i == 2)
-				first_process(fd[0], fd[1], av[1]);
-			else if (i + 1 == n)
-				last_process(fd_in, av[ac - 1]);
-			else
-				middle_process(fd_in, fd[1]);
-			close(fd[0]);
+			child_process(x, av, i, n, ac);
 			ft_process(av[i], env);
 		}
 		else
 		{
 			wait(NULL);
-			close(fd[1]);
-			fd_in = fd[0];
-			i++;
+			parent_process(x, &i);
+			//close(x.fd[1]);
+			x.fd_in = x.fd[0];
+			//i++;
 		}
 	}
 	return (1);
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: naverbru <naverbru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:41:42 by naverbru          #+#    #+#             */
-/*   Updated: 2022/04/14 17:39:33 by naverbru         ###   ########.fr       */
+/*   Updated: 2022/04/19 12:40:21 by naverbru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft/libft.h"
+
+typedef struct t_file{
+	int fd[2];
+	int fd_in;
+	int fd_out;
+	int pid;
+}			t_file;
 
 //utils.c
 int		ft_process(char *av, char **env);
